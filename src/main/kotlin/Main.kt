4402e0077fd7830fe4968e4ebabfabd61e5b5100
wilinzzz <wilinzzz@outlook.com>
2025-09-03 20:41:30 +0800

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,6 +11,6 @@ fun main() {
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+        println("i = $i ")
     }
 }
